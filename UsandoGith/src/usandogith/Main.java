26a79de242e572671d335dad53aff921ac9443da
bryan21102004 <164291771+bryan21102004@users.noreply.github.com>
2024-04-07 11:38:31 -0600

    Genere suma de dos numeros

---
+++ UsandoGith/src/usandogith/Main.java
@@ -0,0 +1,14 @@
+
+package usandogith;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    
+    UsandoGith m= new UsandoGith(4, 4);
+        System.out.println("La suma es: " + m.suma());
+    
+            }
+}
+
--- UsandoGith/src/usandogith/UsandoGith.java
+++ UsandoGith/src/usandogith/UsandoGith.java
@@ -1,21 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package usandogith;
 
-/**
- *
- * @author Usuario
- */
+
 public class UsandoGith {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola pipe");
-        System.out.println("Dos mas dos es tres");
+   
+ private int numeroUno;
+private int numeroDos;
+
+    public UsandoGith(int numeroUno, int numeroDos) {
+        this.numeroUno = numeroUno;
+        this.numeroDos = numeroDos;
     }
+
+public int suma(){
+     return numeroUno + numeroDos;
+}
     
 }
